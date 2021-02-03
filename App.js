60838f2b43b79d7118aca095dff45f02ae268186
jeongjiwoo-include <jjw89506343@gmail.com>
2021-02-04 01:09:45 +0900

    EPSG:5186 -> LONG/LAT convert and show on console

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { View, Text, PermissionsAndroid } from "react-native";
 import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 import Geolocation from 'react-native-geolocation-service';
+import { registerRootComponent } from "expo";
+import XYdata from './data.json';
 
 async function requestPermissions() {
   if (Platform.OS === 'ios') {
@@ -19,6 +21,19 @@ function App() {
   const [locations, setLocations] = useState([]);
   let _watchId;
 
+//proj4
+  var proj4 = require("proj4")
+
+  const epsg5186 = "+proj=tmerc +lat_0=38 +lon_0=127 +k=1 +x_0=200000 +y_0=600000 +ellps=GRS80 +units=m +no_defs";
+  const wgs84 = "+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees";
+
+  XYdata.DATA.map((item)=>{
+    console.log(`X좌표 : ${item.xce} / Y좌표 : ${item.yce}`);
+    var new_data = proj4(epsg5186, wgs84, [item.xce, item.yce]);
+    console.log(`위도,경도 : ${new_data}`);
+  }) 
+  // 이 좌표에 위도, 경도를 새로운 json파일로 만들어야함
+//proj4
   useEffect(() => { requestPermissions(); });
   useEffect(() => {
     _watchId = Geolocation.watchPosition(
